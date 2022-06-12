127e7782e8ecd41966e2de355bc4e07cd2874933
Michael Castaño <python.andres.castao@gmail.com>
2022-06-12 04:15:51 -0500

    Commit1

---
+++ proyecto1/Conversor.py
@@ -0,0 +1,8 @@
+peso=input("¿Cuántos pesos colombianos tiene?: ")
+peso= float (peso)
+valor_dolar=3875
+dolares = peso/valor_dolar
+dolares = round(dolares,2)
+dolares =str(dolares)
+print("Tienes "+dolares+" dólares")
+5800
